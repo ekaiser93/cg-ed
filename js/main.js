3824d02261c40d6f19f6f2cb1dc85f8a5ad2d517
ekais <e.kaiser.93@gmail.com>
2017-10-01 11:10:54 -0500

    added new functions to my javascript, prompting user to enter a rating and then asking them for a department they wish to know the courses in.

--- js/main.js
+++ js/main.js
@@ -1,26 +1,93 @@
 console.log("This file is linked!");
-var teacherOne = "Cosmo Llama";
-var deptName = "Science";
-var ratingOne = 2.7;
-var ratingTwo = 3.3;
-var ratingThree = 5.0;
-var avgRating = (ratingOne + ratingTwo + ratingThree)/3;
-var result = Math.round(avgRating*10)/10;
-console.log("Teacher: " + teacherOne + "\n" + "Department: " + deptName + "\n" + "Ratings: " + ratingOne + ", " + ratingTwo + ", " + ratingThree + "\n" + "This teacher has an average rating of: " + result);
+var teacherName = ["Cosmo Llama", "Freddy", "Dolly Parton", "Pedro", "Cheese"];
+var deptName = ["Science", "English", "Chorus", "Gym Class", "Art"];
 
-var studentOne = "Emma Llamaface";
-var semester = "Fall 2017";
-var email = "emmal@cg.edu";
-var courses = "Calculus" + "\n" +  "\t\t" + "Econ 101";
-var gpaOne = 3.2;
-var gpaTwo = 4.2;
-var gpaThree = 4.7;
-var avgGPA = (gpaOne + gpaTwo + gpaThree)/3;
-var finalGPA = Math.round(avgGPA*10)/10;
-console.log("Student Name: " + studentOne + "\n" + "Semester: " + semester + "\n" + "Email: " + email + "\n" + "Courses: " + courses + "\n" + "Average GPA: " + finalGPA);
+/* rating list section */
 
-var courseOne = "Astronomy";
-var deptOne = "Physics";
-var professor = "Sally Jones";
-var semesterName = "Fall 2017";
-console.log("Course Name: " + courseOne + "\n" + "Department Name: " + deptOne + "\n" + "Professor Name: " + professor + "\n" + "Semester: " + semesterName);
+var ratingList = [3.78, 2.12, 2.3, 2.1];
+
+function getAvgRating (ratingsOfTeach) {
+  var total = 0, i;
+  for (var i = 0; i < ratingsOfTeach.length; i ++){
+    total += ratingList[i];
+    var answer = total/ratingsOfTeach.length;
+  }
+  return Math.round((answer)*10)/10;
+}
+
+var avgRating = (getAvgRating(ratingList));
+console.log ("The average teacher rating is " + avgRating);
+
+function addTeacherRating () {
+  ratingList.push(userRating);
+  return ratingList;
+}
+
+
+/*get rating problem - prompt user for rating and return new avg */
+
+var gotRating=false;
+
+var userRating = parseFloat(prompt("We would like for you to review. Please enter a rating between 0.0 - 5.0."));
+console.log(userRating);
+
+
+while (gotRating==false) {
+
+  if (userRating<=5){
+    var newRatingsList = addTeacherRating(userRating);
+    console.log(newRatingsList);
+    var newAvg = getAvgRating(newRatingsList);
+    gotRating=true;
+    alert("Thanks! The new average for this teacher is " + newAvg);
+  }
+
+  else {
+    var userRating = parseFloat(prompt("That is not an acceptable answer. Please enter a rating under 5.0."));
+  }
+
+}
+
+//filter course name section//
+
+/*1-Create a function that returns a the courses only for a department
+specified in the paramenter of the function*/
+
+//here is my course selection according to department list//
+
+var courseNameAndDept = [
+  ['Intro to Physics','Physics'],
+  ['Geometry I', 'Math'],
+]
+
+//start of funtion//
+
+function getCourseSelection (courseArray,userDeptInput) {
+
+    for (var i = 0; i <= courseArray.length; i++) {
+      if (courseArray[i][1]===userDeptInput) {
+        return courseArray[i][0];
+      }
+    }
+}
+
+/*okay, now I will be prompting the user for a department and checking to see
+if their answer is valid.  Then, I will use my getCourseSelection function to
+get the courses and return it to them.*/
+
+var gotDept = false;
+
+while (gotDept==false){
+
+    var userDept = prompt("Please enter a department name you wish to know the courses in.");
+
+    if (typeof userDept === 'string') {
+      var totalSelection = getCourseSelection(courseNameAndDept, userDept);
+      alert("The courses offered are " + totalSelection + ".");
+      gotDept=true;
+    }
+
+    else {
+      var userDept = prompt("That is not a valid entry.  Please enter a department.")
+    }
+  }
